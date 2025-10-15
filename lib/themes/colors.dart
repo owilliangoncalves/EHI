913af21cd8768ac9410274a82a88c7c829f513a6
Willian Gonçalves <williangoncalves74@gmail.com>
2025-10-15 09:06:48 -0300

    adicionado pasta de temas

---
+++ lib/themes/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+const azul60 = Color(0xe600539a);
+const azul25 = Color(0xcc0096d2);
+const azul15 = Color(0xcc00dbff);
+const cinzak90 = Color(0xe6241f21);
+const cinzaK70 = Color(0xe6ababab);
+const cinzaK40 = Color(0xe6bababa);
+const cinzak20 = Color(0xe6d1d4d4);
+const cinzaK10 = Color(0xe6ededed);
+const cinzaK5 = Color(0xfcf2f2f2);
---
+++ lib/themes/texts.dart
@@ -0,0 +1,1 @@
+String homeTitle = 'Página Inicial - Login';
